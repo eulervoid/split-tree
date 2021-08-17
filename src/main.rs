16b38d2f704e10c027adfed2d82b8def210a58ea
Joshua Peschke <joshua.peschke@gmail.com>
2021-08-17 20:13:20 +0200

    Removed texture from this branch, see poster branch for textured variant

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ fn main() {
 }
 
 struct Model {
-    texture: Option<wgpu::Texture>,
     tree: SplitTree,
     num_lines: u32,
     frames_per_cycle: u64,
@@ -25,12 +24,8 @@ fn model(app: &App) -> Model {
         .msaa_samples(4)
         .build()
         .unwrap();
-    let assets = app.assets_path().unwrap();
-    let img_path = assets.join("posters").join("poster2.jpeg");
-    let texture = wgpu::Texture::from_path(app, img_path).ok();
 
     Model {
-        texture,
         tree: SplitTree::random(10),
         num_lines: 15,
         frames_per_cycle: 600,
@@ -70,64 +65,23 @@ fn view(app: &App, model: &Model, frame: Frame) {
             )
         };
 
-        if let Some(_tex) = &model.texture {
-            let n = (model.num_lines as f32)
-                .min(5.0 * abs((rect.w() / rect.h()) - (rect.h() / rect.w())));
-
-            if rect.w() > 10.0 && rect.h() > 10.0 {
-                for i in 0..(n.ceil() as u32) {
-                    draw.line()
-                        .weight(1.0)
-                        .color(WHITE)
-                        .start(Vec2::lerp(a, b, i as f32 / n as f32))
-                        .end(Vec2::lerp(c, d, i as f32 / n as f32));
-                }
-
-                /*
-                let [w, h] = tex.size();
-                let ratio = w as f32 / h as f32;
-                let tex_bounds = if rect.w() < rect.h() && rect.w() * (1.0 / ratio) < rect.h() {
-                    Rect::from_w_h(rect.w(), rect.w() * (1.0 / ratio))
-                        .align_left_of(rect)
-                        .align_top_of(rect)
-                } else {
-                    Rect::from_w_h(rect.h() * ratio, rect.h())
-                        .align_left_of(rect)
-                        .align_top_of(rect)
-                };
-
-                draw.texture(tex)
-                    .wh(tex_bounds.wh())
-                    .xy(tex_bounds.xy())
-                    .area(Rect::from_x_y_w_h(0.5, 0.5, 1.0, 1.0));
-                */
+        let n =
+            (model.num_lines as f32).min(5.0 * abs((rect.w() / rect.h()) - (rect.h() / rect.w())));
+        if rect.w() > 10.0 && rect.h() > 10.0 {
+            for i in 0..(n.ceil() as u32) {
+                draw.line()
+                    .weight(1.0)
+                    .color(WHITE)
+                    .start(Vec2::lerp(a, b, i as f32 / n as f32))
+                    .end(Vec2::lerp(c, d, i as f32 / n as f32));
             }
-
-            draw.rect()
-                .xy(rect.xy())
-                .wh(rect.wh())
-                .stroke(WHITE)
-                .stroke_weight(2.0)
-                .no_fill();
-        } else {
-            let n = (model.num_lines as f32)
-                .min(5.0 * abs((rect.w() / rect.h()) - (rect.h() / rect.w())));
-            if rect.w() > 10.0 && rect.h() > 10.0 {
-                for i in 0..(n.ceil() as u32) {
-                    draw.line()
-                        .weight(1.0)
-                        .color(WHITE)
-                        .start(Vec2::lerp(a, b, i as f32 / n as f32))
-                        .end(Vec2::lerp(c, d, i as f32 / n as f32));
-                }
-            }
-            draw.rect()
-                .xy(rect.xy())
-                .wh(rect.wh())
-                .stroke(BLACK)
-                .stroke_weight(4.0)
-                .no_fill();
         }
+        draw.rect()
+            .xy(rect.xy())
+            .wh(rect.wh())
+            .stroke(BLACK)
+            .stroke_weight(4.0)
+            .no_fill();
     }
 
     draw.to_frame(app, &frame).unwrap();
